da31552dc1cffd3b500cf7b3da396b2b7d6d1799
Emil Persson <emilpersson32@hotmail.com>
2019-10-30 15:57:11 +0100

    Everything works, Pre constraints

---
+++ cvApp/ExperienceViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ExperienceViewController.swift
+//  cvApp
+//
+//  Created by Emil Persson on 2019-10-30.
+//  Copyright © 2019 Emil Persson. All rights reserved.
+//
+
+import UIKit
+
+class ExperienceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
