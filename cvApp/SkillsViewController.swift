4af14d650476ce5afd358ebc26f1ef0ff1a16edd
Emil Persson <peem18cz@mace2432-20.jth-stud2.hj.se>
2019-11-04 13:55:29 +0100

    added new view, done cvView

---
+++ cvApp/SkillsViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SkillsViewController.swift
+//  cvApp
+//
+//  Created by Emil Persson on 2019-11-04.
+//  Copyright © 2019 Emil Persson. All rights reserved.
+//
+
+import UIKit
+
+class SkillsViewController: UIViewController {
+let backgroundImageView = UIImageView()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setBackground()
+        // Do any additional setup after loading the view.
+    }
+    func setBackground() {
+        view.addSubview(backgroundImageView)
+        backgroundImageView.translatesAutoresizingMaskIntoConstraints = false
+        backgroundImageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        backgroundImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        backgroundImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        backgroundImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        
+        backgroundImageView.image = UIImage(named: "background")
+        view.sendSubviewToBack(backgroundImageView)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
